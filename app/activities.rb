db459842cf0bb2ef565d882b901221eb57c4c9fb
Jim van Musscher <jim.van.musscher@gmail.com>
2015-07-09 00:10:31 +0200

    Move ActivityStore inside Activities class.

--- app/activities.rb
+++ app/activities.rb
@@ -1,7 +1,5 @@
 require 'yaml/store'
 
-ActivityStore = YAML::Store.new("activities.yml")
-
 class Activity
   def initialize(name)
     @name = name
@@ -13,40 +11,44 @@ end
 class Activities
   def self.activities
     roots.inject([]) do |result, name|
-      activity = ActivityStore.transaction { ActivityStore[name] }
+      activity = store.transaction { store[name] }
 
       result << activity
     end
   end
 
   def self.delete(name)
-    ActivityStore.transaction { ActivityStore.delete name }
+    store.transaction { store.delete name }
   end
 
   def self.create(name)
     activity = Activity.new(name)
 
-    ActivityStore.transaction { ActivityStore[name] = activity }
+    store.transaction { store[name] = activity }
   end
 
   def self.replace(name, new_name)
     new_activity = Activity.new(new_name)
 
-    ActivityStore.transaction do
-      ActivityStore.delete name
+    store.transaction do
+      store.delete name
 
-      ActivityStore[new_name] = new_activity
+      store[new_name] = new_activity
     end
   end
 
   def self.find(name)
-    ActivityStore.transaction { ActivityStore[name] }
+    store.transaction { store[name] }
   rescue PStore::Error
     false
   end
 
   private
     def self.roots
-      @roots ||= ActivityStore.transaction { ActivityStore.roots }
+      @roots ||= store.transaction { store.roots }
+    end
+
+    def self.store
+      @store ||= YAML::Store.new("activities.yml")
     end
 end
