02017dc8741821c11ec43409930ca6c2dbb41f4d
Jim van Musscher <jim.van.musscher@gmail.com>
2015-07-17 18:11:32 +0200

    Remove database related code from Cucumber configuration.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,17 +1,9 @@
 require 'childprocess'
 require 'sequel'
 
-DB = Sequel.postgres 'todo_test'
-
-DB.create_table :activities do
-  primary_key :id
-  String :name
-end
-
 server = ChildProcess.build "ruby", "webmachine_application.rb"
 server.start
 
 at_exit do
   server.stop
-  DB.drop_table :activities
 end
