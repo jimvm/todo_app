f7198d9b561fe858a362e0d8eb92f29ea39d49d4
Jim van Musscher <jim.van.musscher@gmail.com>
2015-08-05 00:42:05 +0200

    Move Account related association specs to account_spec.rb

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,15 +1,15 @@
 require_relative "../app/persistence"
 
+def slug
+  Account.create_slug
+end
+
 RSpec.describe Account do
   after(:each) do
     db = Sequel.postgres "todo_test"
 
     db[:activities].delete
     db[:accounts].delete
-  end
-
-  def slug
-    Account.create_slug
   end
 
   describe ".verify" do
@@ -125,6 +125,50 @@ RSpec.describe Account do
           Account.create name: "myname", url_slug: "just_eleven"
         end.to raise_error Sequel::ValidationFailed, "url_slug is not 12 characters"
       end
+    end
+  end
+end
+
+RSpec.context "Accounts with Activities" do
+  after(:each) do
+    db = Sequel.postgres "todo_test"
+
+    db[:activities].delete
+    db[:accounts].delete
+  end
+
+  let(:account)           { Account.create name: "somename", url_slug: slug, password_hash: "test" }
+  let(:different_account) { Account.create name: "othername", url_slug: slug, password_hash: "test" }
+
+  before do
+    account.add_activity Activity.new description: "somedescription", url_slug: slug
+    account.save
+  end
+
+  context "database constraint" do
+    it "ensures accounts can not have activities with the same description" do
+      db = Sequel.postgres "todo_test"
+
+      expect do
+        db[:activities].insert description: "somedescription", account_id: account.id, url_slug: slug
+      end.to raise_error Sequel::UniqueConstraintViolation
+    end
+
+    it "ensures different accounts can have an activity with the same description" do
+      expect do
+        different_account.add_activity Activity.new description: "somedescription", url_slug: slug
+        different_account.save
+      end.not_to raise_error
+    end
+  end
+
+  context "model validation" do
+    it "ensures accounts can not have activities with the same description" do
+      expect do
+        account.add_activity Activity.new description: "somedescription", url_slug: slug
+        account.save
+      end.to raise_error Sequel::ValidationFailed,
+        "description and account_id is already taken"
     end
   end
 end
